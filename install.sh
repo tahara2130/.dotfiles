46378e4c9bd740e23d72d0271ee5652d3094212c
Nahuel Tahara <ntahara2130@gmail.com>
2022-12-17 08:07:18 -0300

    Changes!!! D:

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-#Neovim config
+#Neovim Config
+rm -rf ~/.config/nvim
 ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
+#Fish Config
+rm -rf ~/.config/fish
+ln -sf ~/.dotfiles/.config/fish ~/.config/fish
